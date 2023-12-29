ffac16332dff37aab0696e4ef08aedf36cd5e091
vtlinh86 <vtlinh86@gmail.com>
2023-12-29 12:50:07 +0700

    Delete cocos-vr-example directory

--- cocos-vr-example/cocos-js/bullet.wasm-3bef7842.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e,t){"use strict";return{execute:function(){e("default",new URL("assets/bullet.wasm-9d17e9bf.wasm",t.meta.url).href)}}}));
--- cocos-vr-example/cocos-js/wait-for-ammo-instantiation.js
+++
@@ -1,1 +0,0 @@
-System.register(["./instantiated-3396e353.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.i9)}],execute:function(){}}}));
